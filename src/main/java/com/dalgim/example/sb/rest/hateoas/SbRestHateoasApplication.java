c0777efd368f3a81fc8bb0bb50cdcf375195dfe8
dalgim <dalgiewiczmateusz@gmail.com>
2017-05-01 17:07:53 +0200

    Initial commit

---
+++ src/main/java/com/dalgim/example/sb/rest/hateoas/SbRestHateoasApplication.java
@@ -0,0 +1,12 @@
+package com.dalgim.example.sb.rest.hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestHateoasApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dalgim/example/sb/rest/hateoas/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.dalgim.example.sb.rest.hateoas.config;
+
+/**
+ * Created by Mateusz Dalgiewicz on 01.05.2017.
+ */
+public class AppConfig {
+}
