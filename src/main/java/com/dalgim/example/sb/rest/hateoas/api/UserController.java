a8bf4121007b3a10d54e9086be7e19e0415e2e38
dalgim <dalgiewiczmateusz@gmail.com>
2017-05-03 22:50:18 +0200

    Pobieranie wszystkich uzytkownikow

--- src/main/java/com/dalgim/example/sb/rest/hateoas/api/UserController.java
+++ src/main/java/com/dalgim/example/sb/rest/hateoas/api/UserController.java
@@ -7,19 +7,23 @@ import com.dalgim.example.sb.rest.hateoas.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * Created by Mateusz Dalgiewicz on 01.05.2017.
  */
-@RequestMapping("/user")
-@RestController
 @ExposesResourceFor(UserResource.class)
 @RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
     private final UserRepository userRepository;
@@ -27,13 +31,16 @@ public class UserController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserResource> getById(@PathVariable final Long id) {
-        User user = userRepository.findOne(id);
-        UserResource userResource = userResourceAssembler.toResource(user);
+        final User user = userRepository.findOne(id);
+        final UserResource userResource = userResourceAssembler.toResource(user);
         return ResponseEntity.ok(userResource);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Resources<UserResource>> getAll() {
-        return ResponseEntity.ok(null); //todo
+        final Iterable<User> allUser = userRepository.findAll();
+        final List<UserResource> allUserResource = userResourceAssembler.toResources(allUser);
+        final Resources<UserResource> resources = new Resources<>(allUserResource, ControllerLinkBuilder.linkTo(this.getClass()).withSelfRel());
+        return ResponseEntity.ok(resources);
     }
 }
